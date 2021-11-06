0a899c4ac4bc785136a138cc47302f83c06c2b4d
Felicitas Peredo <felicitasperedo97@gmail.com>
2021-11-06 11:23:36 -0300

    listaProductos Update

--- listaProductos.js
+++ listaProductos.js
@@ -1,46 +1,200 @@
-let listaProductos = [{
-        "id": 1,
+let listaProductos = [
+    //AROS
+    {
+        "id": "AROS1",
         "nombre": "Aros Afrika",
         "categoria": "Aros",
         "material": "Oro",
         "precio": 2500,
+        "imagenProducto": "https://i.ibb.co/ckjrGS1/Aros-Argollas-Oro.jpg",
         "stock": 50
     }, {
-        "id": 2,
-        "nombre": "Collar Indica",
-        "categoria": "Collares",
+        "id": "AROS2",
+        "nombre": "Aros Afrika",
+        "categoria": "Aros",
+        "material": "Plata",
+        "precio": 2500,
+        "imagenProducto": "https://i.ibb.co/cvDK7yY/Aros-Argollas-Plata.jpg",
+        "stock": 50
+    }, {
+        "id": "AROS3",
+        "nombre": "Aros Oceania",
+        "categoria": "Aros",
         "material": "Oro",
-        "precio": 3500,
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/znzZHJy/Aros-Brillos.jpg",
         "stock": 50
     }, {
-        "id": 3,
+        "id": "AROS4",
+        "nombre": "Aros Tavira",
+        "categoria": "Aros",
+        "material": "Oro",
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/ZVLjsCB/Aros-Circulo-Oro.jpg",
+        "stock": 50
+    }, {
+        "id": "AROS5",
+        "nombre": "Aros Tavira",
+        "categoria": "Aros",
+        "material": "Plata",
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/F8ZGNrL/Aros-Circulo-Plata.jpg",
+        "stock": 50
+    }, {
+        "id": "AROS6",
+        "nombre": "Aros Evora",
+        "categoria": "Aros",
+        "material": "Oro",
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/BcGbmxX/Aros-Doble-Argollita-Oro.jpg",
+        "stock": 50
+    }, {
+        "id": "AROS7",
+        "nombre": "Aros Evora",
+        "categoria": "Aros",
+        "material": "Plata",
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/M5vtK6s/Aros-Doble-Argollita-Plata.jpg",
+        "stock": 50
+    }, {
+        "id": "AROS8",
+        "nombre": "Aros Nova",
+        "categoria": "Aros",
+        "material": "Oro",
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/NZ2ZnSz/Aros-Perlita.jpg",
+        "stock": 50
+    }, {
+        "id": "AROS9",
+        "nombre": "Aros Meia",
+        "categoria": "Aros",
+        "material": "Oro",
+        "precio": 2700,
+        "imagenProducto": "https://i.ibb.co/BP038tj/Aros-Rayito.jpg",
+        "stock": 50
+    },
+    //ANILLOS
+    {
+        "id": "ANILLOS1",
         "nombre": "Anillo Egeo",
         "categoria": "Anillos",
         "material": "Oro",
         "precio": 2000,
+        "imagenProducto": "https://i.ibb.co/pbV2b7d/Anillo-Brillos-Oro.jpg",
         "stock": 50
     }, {
-        "id": 4,
-        "nombre": "Aros Oceania",
-        "categoria": "Aros",
-        "material": "Plata",
-        "precio": 2700,
-        "stock": 50
-    }, {
-        "id": 5,
-        "nombre": "Collar Asia",
-        "categoria": "Collares",
-        "material": "Plata",
-        "precio": 3900,
-        "stock": 50,
-    }, {
-        "id": 6,
+        "id": "ANILLOS2",
         "nombre": "Anillo Artico",
         "categoria": "Anillos",
         "material": "Plata",
         "precio": 2300,
+        "imagenProducto": "https://i.ibb.co/XzZWqfK/Anillo-Gaia-Oro.jpg", 
         "stock": 50 
+    }, {
+        "id": "ANILLOS3",
+        "nombre": "Anillo Gaia",
+        "categoria": "Anillos",
+        "material": "Plata",
+        "precio": 2300,
+        "imagenProducto": "https://i.ibb.co/wp5pzSz/Anillo-Runa-Oro.jpg", 
+        "stock": 50 
+    }, 
+    //COLLARES
+    {
+        "id": "COLLARES1",
+        "nombre": "Collar Asia",
+        "categoria": "Collares",
+        "material": "Oro",
+        "precio": 3900,
+        "imagenProducto": "https://i.ibb.co/s1W7Qwt/Collar-Circulo-Oro.jpg",
+        "stock": 50,
+    }, {
+        "id": "COLLARES2",
+        "nombre": "Collar Indica",
+        "categoria": "Collares",
+        "material": "Oro",
+        "precio": 3500,
+        "imagenProducto": "https://i.ibb.co/QK4mzK3/Collar-Grueso-Oro.jpg",
+        "stock": 50
+    }, {
+        "id": "COLLARES3",
+        "nombre": "Collar Indica",
+        "categoria": "Collares",
+        "material": "Plata",
+        "precio": 3500,
+        "imagenProducto": "https://i.ibb.co/Fb9vpNs/Collar-Grueso-Plata.jpg",
+        "stock": 50
+    }, {
+        "id": "COLLARES4",
+        "nombre": "Collar Amora",
+        "categoria": "Collares",
+        "material": "Plata",
+        "precio": 3700,
+        "imagenProducto": "https://i.ibb.co/tZdHnNM/Collar-Corazon-Plata.jpg",
+        "stock": 50,
+    }, {
+        "id": "COLLARES5",
+        "nombre": "Collar Oporto",
+        "categoria": "Collares",
+        "material": "Oro",
+        "precio": 3900,
+        "imagenProducto": "https://i.ibb.co/4gxSCRC/Collar-Cuadrado-Plata.jpg",
+        "stock": 50,
+    }, {
+        "id": "COLLARES6",
+        "nombre": "Collar Lisboa",
+        "categoria": "Collares",
+        "material": "Oro",
+        "precio": 3900,
+        "imagenProducto": "https://i.ibb.co/5W27LSG/Collar-Dije-Oro.jpg",
+        "stock": 50,
+    }, {
+        "id": "COLLARES7",
+        "nombre": "Collar Laos",
+        "categoria": "Collares",
+        "material": "Oro",
+        "precio": 3900,
+        "imagenProducto": "https://i.ibb.co/HYpfpMC/Collar-Dije-Perla.jpg",
+        "stock": 50,
+    }, {
+        "id": "COLLARES8",
+        "nombre": "Collar Serp",
+        "categoria": "Collares",
+        "material": "Oro",
+        "precio": 3900,
+        "imagenProducto": "https://i.ibb.co/tzD9QXC/Collar-Serpiente-Oro.jpg",
+        "stock": 50,
+    }, {
+        "id": "COLLARES9",
+        "nombre": "Collar Sintra",
+        "categoria": "Collares",
+        "material": "Plata",
+        "precio": 3900,
+        "imagenProducto": "https://i.ibb.co/3rwpW3b/Collar-Mini-Plata.jpg",
+        "stock": 50,
     }
 ]
 
 localStorage.setItem("listaProductos", JSON.stringify(listaProductos));
+
+//<img src="https://i.ibb.co/pbV2b7d/Anillo-Brillos-Oro.jpg" alt="Anillo-Brillos-Oro" border="0">
+//<img src="https://i.ibb.co/XzZWqfK/Anillo-Gaia-Oro.jpg" alt="Anillo-Gaia-Oro" border="0">
+//<img src="https://i.ibb.co/wp5pzSz/Anillo-Runa-Oro.jpg" alt="Anillo-Runa-Oro" border="0"></img>
+//<img src="https://i.ibb.co/ckjrGS1/Aros-Argollas-Oro.jpg" alt="Aros-Argollas-Oro" border="0">
+//<img src="https://i.ibb.co/cvDK7yY/Aros-Argollas-Plata.jpg" alt="Aros-Argollas-Plata" border="0">
+//<img src="https://i.ibb.co/znzZHJy/Aros-Brillos.jpg" alt="Aros-Brillos" border="0">
+//<img src="https://i.ibb.co/ZVLjsCB/Aros-Circulo-Oro.jpg" alt="Aros-Circulo-Oro" border="0">
+//<img src="https://i.ibb.co/F8ZGNrL/Aros-Circulo-Plata.jpg" alt="Aros-Circulo-Plata" border="0">
+//<img src="https://i.ibb.co/BcGbmxX/Aros-Doble-Argollita-Oro.jpg" alt="Aros-Doble-Argollita-Oro" border="0">
+//<img src="https://i.ibb.co/M5vtK6s/Aros-Doble-Argollita-Plata.jpg" alt="Aros-Doble-Argollita-Plata" border="0">
+//<img src="https://i.ibb.co/NZ2ZnSz/Aros-Perlita.jpg" alt="Aros-Perlita" border="0">
+//<img src="https://i.ibb.co/BP038tj/Aros-Rayito.jpg" alt="Aros-Rayito" border="0"></img>
+//<img src="https://i.ibb.co/s1W7Qwt/Collar-Circulo-Oro.jpg" alt="Collar-Circulo-Oro" border="0">
+//<img src="https://i.ibb.co/tZdHnNM/Collar-Corazon-Plata.jpg" alt="Collar-Corazon-Plata" border="0">
+//<img src="https://i.ibb.co/4gxSCRC/Collar-Cuadrado-Plata.jpg" alt="Collar-Cuadrado-Plata" border="0">
+//<img src="https://i.ibb.co/5W27LSG/Collar-Dije-Oro.jpg" alt="Collar-Dije-Oro" border="0">
+//<img src="https://i.ibb.co/HYpfpMC/Collar-Dije-Perla.jpg" alt="Collar-Dije-Perla" border="0">
+//</img></img><img src="https://i.ibb.co/QK4mzK3/Collar-Grueso-Oro.jpg" alt="Collar-Grueso-Oro" border="0">
+//</img></img><img src="https://i.ibb.co/Fb9vpNs/Collar-Grueso-Plata.jpg" alt="Collar-Grueso-Plata" border="0">
+//<img src="https://i.ibb.co/3rwpW3b/Collar-Mini-Plata.jpg" alt="Collar-Mini-Plata" border="0">
+//<img src="https://i.ibb.co/tzD9QXC/Collar-Serpiente-Oro.jpg" alt="Collar-Serpiente-Oro" border="0"></img>
